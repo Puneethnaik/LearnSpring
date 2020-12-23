3fc8246a08b0bdd75af3ea9df3c2a0ee79bc267c
puneeth <puneethn250@gmail.com>
2020-12-23 20:25:18 +0530

    spring application context using FileSystemApplicationContext

---
+++ spring-demo-one/springDemoOne/src/main/java/com/example/App.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.io.File;
+
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {        
+        //load the spring configuration file
+        FileSystemXmlApplicationContext context = new FileSystemXmlApplicationContext("./applicationContext.xml");
+        //retrieve bean from spring container
+        Coach coach = context.getBean("myCoach", Coach.class);
+        //call methods on the bean
+        System.out.println(coach.getDailyWorkout());
+        //close the context
+        context.close();
+    }
+}
---
+++ spring-demo-one/springDemoOne/src/main/java/com/example/Coach.java
@@ -0,0 +1,4 @@
+package com.example;
+interface Coach {
+    public String getDailyWorkout();
+}
